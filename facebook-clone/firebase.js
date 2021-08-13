432df34f4d9d625a0398d44d0644f2ed83850e89
chalana kalpitha <chalanakalpitha72@gmail.com>
2021-08-13 14:42:54 +0530

    change configuration and consoles

--- facebook-clone/firebase.js
+++ facebook-clone/firebase.js
@@ -3,12 +3,7 @@ import 'firebase/storage';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCppgxLBMfm0UnepXvL7e9rxiPwDhn-IvM",
-    authDomain: "facebook-clone-ck.firebaseapp.com",
-    projectId: "facebook-clone-ck",
-    storageBucket: "facebook-clone-ck.appspot.com",
-    messagingSenderId: "767142014675",
-    appId: "1:767142014675:web:fc7f4a9cf89f08d025ee26"
+//   Add your firebase project configuration data
   };
 
 
